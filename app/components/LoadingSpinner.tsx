d7a07b5155e5245d25c61c52e5d90ff0dbc647d6
PradyLinux <pradyumna.p.6969@gmail.com>
2024-10-26 18:01:08 +0530

    Refactor LoadingSpinner to use Next.js Image for optimized loading and improved performance

--- app/components/LoadingSpinner.tsx
+++ app/components/LoadingSpinner.tsx
@@ -1,9 +1,19 @@
-import React from 'react';
+import Image from "next/image";
+import React from "react";
 
 const LoadingSpinner = () => {
   return (
     <div className="fixed inset-0 flex justify-center items-center bg-white bg-opacity-50 backdrop-blur-sm z-50">
-      <img src="/om1.png" alt="Loading..." className="w-22 h-22 animate-spin" />
+      <div className="w-22 h-22">
+        <Image
+          src="/om1.png"
+          alt="Loading..."
+          width={88} // Set an appropriate width
+          height={88} // Set an appropriate height
+          className="animate-spin"
+          priority // Ensures the image loads as quickly as possible
+        />
+      </div>
     </div>
   );
 };
