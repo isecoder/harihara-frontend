d929726769d2b3f81c039719ff4be0fdc68f925c
hrithikshetty <hrithik.cd21@sahyadri.edu.in>
2024-10-29 17:20:23 +0530

    home page made responsive

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,7 @@ import { useSelector, useDispatch } from "react-redux";
 import { RootState, AppDispatch } from "./store";
 import { changeLocale } from "./store/localeSlice";
 import Hero from "./components/hero";
-import LanguageSwitcher from "./components/LanguageSwitcher";
+// import LanguageSwitcher from "./components/LanguageSwitcher";
 
 export default function Home() {
   const dispatch = useDispatch<AppDispatch>();
@@ -26,19 +26,16 @@ export default function Home() {
   return (
     <main className="min-h-screen flex flex-col items-center p-8 text-center">
       <Hero />
-      <h1>{welcome}</h1>
-      <p>{language}</p>
-      <p>{greeting}</p>
+      <p className="text-4xl font-bold text-orange-500 pt-7">Welcome to our Site</p>
       <main className="container mx-auto p-4">
         <h2 className="text-3xl font-bold mb-4">Donation</h2>
         <div className="bg-white p-6 rounded-lg shadow-md">
           <h3 className="text-2xl font-bold text-orange-500 mb-4">ACCOUNT DETAILS</h3>
-          <p className="text-xl mb-2">Bank of Baroda</p>
-          <p className="mb-2">Account Number: 131101011001504</p>
-          <p>IFSC Code: VIJB0001311</p>
+          <p className="text-2xl mb-2">Bank of Baroda</p>
+          <p className="mb-2 text-2xl">Account Number: 131101011001504</p>
+          <p className="text-2xl">IFSC Code: VIJB0001311</p>
         </div>
       </main>
-      <LanguageSwitcher />
     </main>
   );
 }
