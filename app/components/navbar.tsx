1db4e1736ff56bbc2645cfe8b313d45c9f4acfc8
Skanda Ganesh P V <49191239+Skandaganesh@users.noreply.github.com>
2024-10-31 20:57:20 +0530

    Routing for news

--- app/components/navbar.tsx
+++ app/components/navbar.tsx
@@ -21,14 +21,14 @@ const navLinks: NavLink[] = [
     subLinks: [
       { href: "/history", label: "History" },
       { href: "/temple", label: "About Temple" },
-      { href: "/administration", label: "Administration" },
+      { href: "/admin", label: "Administration" },
       { href: "/how_to_reach", label: "How to Reach" },
       { href: "/nearby_places", label: "Nearby Places" },
     ],
   },
   { href: "/sevas", label: "Sevas" },
   { href: "/donations", label: "Donations" },
-  { href: "/newsupdates", label: "News & Updates" },
+  { href: "/news", label: "News & Updates" },
   { href: "/gallery", label: "Gallery" },
   { href: "/contact", label: "Contact" },
 ]
