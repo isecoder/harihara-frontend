97e0e291f800905d697c30b382d74efc1844a742
Shreekara B <shreekarab5@gmail.com>
2024-11-10 12:13:02 +0530

    Added styles,boxes for seva list

--- app/sevas/page.tsx
+++ app/sevas/page.tsx
@@ -68,38 +68,38 @@ const SevasList = (): JSX.Element => {
   };
 
   if (selectedSeva) {
-    // If a seva is selected, render the SevaForm component
     return <SevaForm seva={selectedSeva} showKannada={showKannada} />;
   }
 
   return (
-    <div className="container mx-auto p-6">
-      {/* Note about contacting the temple office in English and Kannada */}
+    <div className="flex flex-col items-center min-h-screen p-6 bg-gradient-to-b from-white to-orange-100">
       <p className="text-center text-sm font-bold text-red-500 mb-4 animate-pulse shadow-lg">
         {showKannada
           ? "ದಯವಿಟ್ಟು ಸೇವೆಯನ್ನು ನಡೆಸುವ ದಿನಕ್ಕಿಂತ ಕನಿಷ್ಟ ಒಂದು ದಿನದ ಮುಂಚಿತವಾಗಿ ದೃಢೀಕರಣಕ್ಕಾಗಿ ದೇವಾಲಯದ ಕಚೇರಿಯನ್ನು ಸಂಪರ್ಕಿಸಿ."
           : "Please contact the temple office for confirmation regarding the seva at least one day prior to the day of worship."}
       </p>
       {error && <p className="text-red-500 text-center">{error}</p>}
-      {loading && <LoadingSpinner />} {/* Show loading spinner while fetching */}
+      {loading && <LoadingSpinner />} 
       {!loading && sevas.length === 0 && !error && (
         <p className="text-center">No sevas available.</p>
       )}
 
-      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8 mt-10">
+      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8 mt-10 max-w-5xl w-full">
         {sevas.map((seva) => (
           <div
             key={seva.id}
-            className="bg-white shadow-lg rounded-lg p-6 transition-transform transform hover:scale-105 cursor-pointer"
-            onClick={() => handleSevaClick(seva)} // Add onClick to each seva card
+            className="p-6 bg-gradient-to-b from-white to-orange-100 rounded-lg shadow-lg transition-transform transform hover:scale-105 cursor-pointer text-center"
+            onClick={() => handleSevaClick(seva)}
           >
-            <h2 className="text-xl font-semibold text-gray-800">
+            <h2 className="text-xl font-bold text-orange-600 mb-2">
               {showKannada ? seva.name_kannada : seva.name}
             </h2>
-            <p className="text-gray-600 mt-2">
+            <p className="text-gray-700 mt-2">
               {showKannada ? seva.description_kannada : seva.description}
             </p>
-            <p className="mt-4 font-bold text-lg">Price: ₹{seva.base_price}</p>
+            <p className="mt-4 font-semibold text-lg text-gray-800">
+              Price: ₹{seva.base_price}
+            </p>
           </div>
         ))}
       </div>
